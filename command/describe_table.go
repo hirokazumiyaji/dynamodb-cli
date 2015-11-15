ab78f353ea11d9ddb3113e8a3a5a8da5c590315f
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2015-11-15 18:46:51 +0900

    fix describe table command

--- command/describe_table.go
+++ command/describe_table.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/mitchellh/cli"
 )
 
@@ -13,15 +14,30 @@ type DescribeTableCommand struct {
 
 func (c *DescribeTableCommand) Help() string {
 	helpText := `
+Usage: dynamodb-cli describe-table -table-name=<value>
 `
 
 	return strings.TrimSpace(helpText)
 }
 
 func (c *DescribeTableCommand) Run(args []string) int {
+	var tableName string
 	cmdFlags := flag.NewFlagSet("describe-table", flag.ContinueOnError)
 	cmdFlags.Usage = func() { c.Ui.Output(c.Help()) }
+	cmdFlags.StringVar(&tableName, "table-name", "", "table name")
 	if err := cmdFlags.Parse(args); err != nil {
+		c.Ui.Error(err.Error())
+		return 1
+	}
+
+	svc := dynamodb.New(session.New())
+	params := &dynamodb.DescribeTableInput{
+		TableName: tableName,
+	}
+
+	res, err := svc.DescribeTable(params)
+	if err != nil {
+		c.Ui.Error(err.Error())
 		return 1
 	}
 	return 0
